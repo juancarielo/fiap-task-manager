a2d8831c7adf627012dd94d9629e2223c0ca7b35
Juan Carielo <juancarielo@gmail.com>
2021-10-18 22:53:46 -0300

    feat: Add register

--- backend/containers/Login.tsx
+++ backend/containers/Login.tsx
@@ -5,11 +5,17 @@ import { executeRequest } from '../services/api'
 import { AccessTokenProps } from '../types/AccessTokenProps'
 
 const Login: NextPage<AccessTokenProps> = ({ setAccessToken }) => {
-    const [login, setLogin] = useState('juancarielo@gmail.com')
-    const [password, setPassword] = useState('Admin@123')
+    const [name, setName] = useState('')
+    const [login, setLogin] = useState('')
+    const [password, setPassword] = useState('')
+    const [passwordConfirm, setPasswordConfirm] = useState('')
 
     const [msgErro, setMsgErro] = useState('')
+    const [msgSuccess, setMsgSuccess] = useState('')
+
     const [isLoading, setLoading] = useState(false)
+
+    const [isRegister, setRegister] = useState(false)
 
     const doLogin = async (e: any) => {
         try {
@@ -50,6 +56,65 @@ const Login: NextPage<AccessTokenProps> = ({ setAccessToken }) => {
         setLoading(false)
     }
 
+    const doRegister = async (e: any) => {
+        try {
+            setLoading(true)
+            e.preventDefault()
+
+            if (!login || !password) {
+                setMsgErro('Parâmetros de entrada inválidos')
+                setLoading(false)
+                return
+            }
+
+            if (password !== passwordConfirm) {
+                setMsgErro('As senhas não conferem, por favor tente novamente')
+                setLoading(false)
+                return
+            }
+
+            const body = {
+                name,
+                email: login,
+                password,
+            }
+
+            const result = await executeRequest('user', 'POST', body)
+
+            setMsgErro('')
+
+            if (result && result.data) {
+                console.log(result)
+
+                setMsgSuccess('Registro efetuado com sucesso, agora faça login.')
+                setRegister(false)
+
+                setName('')
+                setLogin('')
+                setPassword('')
+                setPasswordConfirm('')
+            } else {
+                setMsgErro('Não foi possível realizar o register, tente novamente!')
+            }
+        } catch (e: any) {
+            // console.log(e);
+            setMsgErro('Ocorreu um erro ao processar o register, tente novamente!')
+        }
+
+        setLoading(false)
+    }
+
+    const handleRegister = (isRegister: boolean) => {
+        if (isRegister) {
+            setRegister(false)
+        } else {
+            setRegister(true)
+        }
+
+        setMsgSuccess('')
+        setMsgErro('')
+    }
+
     return (
         <>
             <Head>
@@ -61,24 +126,81 @@ const Login: NextPage<AccessTokenProps> = ({ setAccessToken }) => {
                 <form>
                     {msgErro && <p>{msgErro}</p>}
 
+                    {msgSuccess && <p className="success">{msgSuccess}</p>}
+
+                    {isRegister && (
+                        <div className="input">
+                            <img src="/mail.svg" alt="Fiap" />
+                            <input
+                                type="text"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                                placeholder="Name"
+                            />
+                        </div>
+                    )}
+
                     <div className="input">
                         <img src="/mail.svg" alt="Fiap" />
-                        <input type="text" value={login} onChange={(e) => setLogin(e.target.value)} />
+                        <input
+                            type="text"
+                            value={login}
+                            onChange={(e) => setLogin(e.target.value)}
+                            placeholder="Login"
+                        />
                     </div>
 
                     <div className="input">
                         <img src="/lock.svg" alt="Fiap" />
-                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="Senha"
+                        />
                     </div>
 
-                    <button
-                        className={isLoading ? 'disabled' : ''}
-                        type="button"
-                        onClick={doLogin}
-                        disabled={isLoading}
-                    >
-                        {isLoading ? 'Carregando...' : 'Login'}
-                    </button>
+                    {!isRegister ? (
+                        <>
+                            <button
+                                className={isLoading ? 'disabled' : ''}
+                                type="button"
+                                onClick={doLogin}
+                                disabled={isLoading}
+                            >
+                                {isLoading ? 'Carregando...' : 'Login'}
+                            </button>
+                        </>
+                    ) : (
+                        <>
+                            <div className="input">
+                                <img src="/lock.svg" alt="Fiap" />
+                                <input
+                                    type="password"
+                                    value={passwordConfirm}
+                                    onChange={(e) => setPasswordConfirm(e.target.value)}
+                                    placeholder="Confirme sua senha"
+                                />
+                            </div>
+
+                            <button
+                                className={isLoading ? 'disabled' : ''}
+                                type="button"
+                                onClick={doRegister}
+                                disabled={isLoading}
+                            >
+                                {isLoading ? 'Registrando...' : 'Register'}
+                            </button>
+                        </>
+                    )}
+
+                    <p className="pt-2">or</p>
+
+                    <p>
+                        <a href="#" onClick={() => handleRegister(isRegister)}>
+                            {isRegister ? 'Login' : 'Register'}
+                        </a>
+                    </p>
                 </form>
             </div>
         </>
