1b53f19b970def9f733e8032c3b62367d94df5a5
Juan Carielo <juancarielo@gmail.com>
2021-11-21 15:19:50 -0300

    feat: Use vue bootstrap

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource';
+import VueBootstrap from 'bootstrap-vue';
 
 import './assets/styles/app.scss';
 
 Vue.use(VueResource);
+Vue.use(VueBootstrap);
 
 Vue.http.options.root = "http://localhost:3000/api/";
 
